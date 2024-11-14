7ee90366dca5b0d6f7306c5f28762dd745fd7b12
brenda127s <brendaserra100@gmail.com>
2024-11-14 14:58:31 -0800

    routes added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect, useRef} from 'react'
+import Resume from './components/Resume'
+import NonExistentPage from './components/NonExistentPage'
+import PromptUser from './components/PromptUser'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
+import { HiArrowRight } from 'react-icons/hi'
 
+
+const converter = require('number-to-words')
 function App() {
+  const [userInfo, setUserInfo] = useState({})
+  const [source, setSource] = useState('')
+  const random_number = Math.floor(Math.random()* 10e4)
+  const random_alpha_num = converter.toWords(random_number)
+  const getRandomNumber = (randNum) => {
+    return converter.toWords(randNum)
+  }
+  const generateImage = () => {
+    setSource(`https://robohash.org/stefan-${random_alpha_num}`)
+    console.log(source)
+  }
+
+  useEffect(() => {
+    generateImage()
+  }, [])
+
+  function handleChange(e) {
+    setUserInfo(prevInfo => {
+      return {
+        ...prevInfo,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path = '/' element = {<PromptUser userObject = {userInfo} imageSource = {source} handleChange = {handleChange}/>}/>
+        <Route path = '/resume' element = {<Resume userObject = {userInfo} imageSource = {source}/>}/>
+        <Route path = '*' element = {<NonExistentPage/>} />
+      </Routes>
+    </Router>
   );
 }
 
